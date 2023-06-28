995aabbe63a61302f1f08eb070742cfa90154572
TylerRice27 <Tylerrayrice2@gmail.com>
2023-06-28 14:53:14 -0600

    Gallery Website Before Mobile

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
 <SideBar />
   <v-main>
     <!-- CARDS -->
+    <ImageGallery />
   </v-main>
  </v-app>
 </template>
 
 <script setup>
+import ImageGallery from './components/ImageGallery.vue';
 import SideBar from './components/SideBar.vue'
 </script>
 
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-row class="pa-2">
+        <v-col :key="n" v-for="n in 198" cols="2">
+            <v-card>
+             <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+             :lazy-src="`https://picsum.photos/500/6?image=${n * 5 + 10}`"
+             aspect-ratio="1" cover>
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular
+                  indeterminate
+                  color="grey-lighten-5"
+                ></v-progress-circular>
+                </v-row>
+            </template>
+            </v-img>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
